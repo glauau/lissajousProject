b38b7b755eb718f4433b6fe9ccfec172ddff0886
Glaucia Lauton <glaucia.brito@maps.com.br>
2024-03-21 01:35:44 -0300

    adding a function to show options

--- lissajous/automation.sh
+++ lissajous/automation.sh
@@ -8,8 +8,43 @@ fi
 
 #function to build image
 build_image(){
+	echo -e "building image, please wait..."
 	docker build -t "$IMAGE_NAME:$IMAGE_VERSION" .
+	sleep 2
+	echo -e "image successfully built."
 }
 
-build_image
+#function to show options
+show_options() {
+echo -e "
+Options:
+1 - Build image
+2 - Push image
+3 - Run image
+4 - Exit
+"
+}
 
+#loop to proccess the informations
+while true; do
+    show_options
+    read -p "choose an option: " opt
+    case $opt in
+        1)
+            build_image
+            ;;
+        2)
+            push_image
+            ;;
+        3)
+            run_image
+            ;;
+        4)
+            echo "exit."
+            break
+            ;;
+        *)
+            echo "invalid option"
+            ;;
+    esac
+done
